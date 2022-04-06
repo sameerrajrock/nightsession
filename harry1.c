9ab29a12ef3698ef5f2dfd93c6840c3346565a5d
sameerrajrock <103138472+sameerrajrock@users.noreply.github.com>
2022-04-06 23:06:24 +0530

    nightses.txt

---
+++ harry1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    
+   int a,b,c,d;
+   printf("enter the divident: ");
+   scanf("%d",&a);
+   printf("enter the divisor: ");
+   scanf("%d",&b);
+   c=a/b;
+   d=a%b;
+   printf("quotient=%d\n",c);
+   printf("remainder=%d\n",d);
+   
+   return 0;
+}
+   
